fe9f9d627fbea548b5b2d8fa2de74246d9107420
jishnupramod <jishnumagnanimous@gmail.com>
2021-01-24 22:13:49 +0530

    solution using priority_queue added

--- 24_merge_k_sorted_lists.cpp
+++ 24_merge_k_sorted_lists.cpp
@@ -71,3 +71,41 @@ public:
     }
 };
 
+
+
+
+// Using priority queue
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* head = new ListNode(0);
+        ListNode* curr = head;
+        auto compare = [&](ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        };
+        priority_queue<ListNode*, vector<ListNode*>, decltype(compare)> pq(compare);
+        for (ListNode* list : lists) {
+            if (list)
+                pq.push(list);
+        }
+        while (!pq.empty()) {
+            curr->next = pq.top();
+            pq.pop();
+            curr = curr->next;
+            if (curr->next)
+                pq.push(curr->next);
+        }
+        return head->next;
+    }
+};
+
